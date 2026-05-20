85b4420e41c2126b37bb0117879a865b53b71803
HectorzBarz <hectorrodriguezbar99@gmail.com>
2026-05-20 15:12:46 +0200

    feat: Create api.php routing

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,27 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProfileController;
+use Illuminate\Http\Request;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+// Rutas públicas
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Rutas protegidas (Requieren enviar el Token en los Headers)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/user/profile', [ProfileController::class, 'show']);
+
+    // Usamos POST en lugar de PUT/PATCH porque la actualización incluye archivos (imágenes)
+    // y PHP maneja mejor los form-data multipart con peticiones POST.
+    Route::post('/user/profile', [ProfileController::class, 'update']);
+});
